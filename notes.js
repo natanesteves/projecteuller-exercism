99e368c120285d56e042ba9dd74ab00ee5093d3e
natan <natan.reis@orange.com>
2022-12-05 12:31:14 -0300

    .

--- notes.js
+++ notes.js
@@ -12,3 +12,21 @@ function calcAge(birthYear) {
 
 const firstName = "Jonas";
 calcAge(1991);
+
+//classes
+// serve para encpsular o seu codigo em pequenas parte/objetos que teram metodos e propiedades
+//começo da classe sempre vai existir o constructor q vai conter as principais propiedades
+
+class Exemplo {
+  constructor(links) {
+    this.linkelemnts = links; //não entendi pq de usar this sempre?
+  }
+}
+
+const exemplo = new Exemplo("a");
+console.log(exemplo.links);
+
+//.this é sempre uma referencia ao objeto geral?
+
+//blz async/await existe pq js é single tread mas como se da o funcionamento do async/await por baixo dos panos?
+//ele simplesmente é executado antes da pilha ou a pilha é parada assim q esse codigo é notado compilador nonbloking?
